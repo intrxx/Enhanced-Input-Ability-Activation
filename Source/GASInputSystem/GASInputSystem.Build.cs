d33316e63704b863be6442d6c0946375a16e0155
intrxx <michaloginski15@gmail.com>
2023-06-26 18:22:54 +0200

    Init

---
+++ Source/GASInputSystem/GASInputSystem.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class GASInputSystem : ModuleRules
+{
+	public GASInputSystem(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+	}
+}
---
+++ Source/GASInputSystem/GASInputSystemGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GASInputSystemGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AGASInputSystemGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGASInputSystemGameMode();
+};
+
+
+
