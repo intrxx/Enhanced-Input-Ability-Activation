a4d3a3dd4c65faf43b45e36947d3458ed39b950c
intrxx <michaloginski15@gmail.com>
2023-06-28 16:21:55 +0200

    filled the copyright notice

--- Source/GASInputSystem/GASInputSystem.cpp
+++ Source/GASInputSystem/GASInputSystem.cpp
@@ -1,4 +1,4 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// Copyright 2023 Michał Ogiński.
 
 #include "GASInputSystem.h"
 #include "Modules/ModuleManager.h"
--- Source/GASInputSystem/GASInputSystem.h
+++ Source/GASInputSystem/GASInputSystem.h
@@ -1,4 +1,4 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// Copyright 2023 Michał Ogiński.
 
 #pragma once
 
